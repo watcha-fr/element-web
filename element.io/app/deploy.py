86e02b97319dd43749f2f9089b2e19fa612d1dcd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 00:35:56 +0000

    Adding numeric_owner as keyword arguement

--- element.io/app/deploy.py
+++ element.io/app/deploy.py
@@ -108,7 +108,7 @@ class Deployer:
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tar, extract_path)
